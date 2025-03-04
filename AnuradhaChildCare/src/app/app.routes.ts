dd9cc0ca60fdc4b5efd0dcc2b8b1e0f8d64e63d2
Satya-1911 <bhogavillisatyasri5@gmail.com>
2025-03-04 21:21:01 +0530

    Added comps and tested routes

--- AnuradhaChildCare/src/app/app.routes.ts
+++ AnuradhaChildCare/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { WorkshopsComponent } from './workshops/workshops.component';
+import { BlogComponent } from './blog/blog.component';
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'Workshops', component: WorkshopsComponent},
+    { path: 'blog', component: BlogComponent}
+];
